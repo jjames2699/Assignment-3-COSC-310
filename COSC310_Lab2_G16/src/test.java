f93691c57f056c3f45e1d065113df56c80c07ba3
Daulton <daultonbaird1@gmail.com>
2019-02-05 18:52:38 -0800

    Update test.java
    my push boi

--- COSC310_Lab2_G16/src/test.java
+++ COSC310_Lab2_G16/src/test.java
@@ -3,6 +3,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Damyn Push");
-		System.out.println("Randy Pull")
+		System.out.println("Randy Pull");
+		System.out.println("Daulton Push");
 	}
 }
