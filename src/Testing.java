3ef5158f4e35c9cddd2e6194ce57df049b4c1975
Daulton <daultonbaird1@gmail.com>
2019-02-08 17:34:06 -0800

    O(n^2^e^x) complexity

--- src/Testing.java
+++ src/Testing.java
@@ -8,9 +8,9 @@ public class Testing {
 		// TODO Auto-generated method stub
 		String user= "";
 		//BotTree bot = new BotTree(0); //This is going to be the initial instance of whatever tree class we make
-		Tree<String> start = new Tree<String>(0);
-		String initialMsg = (String) start.getRootData(); //Root contains the first String
-		System.out.println(initialMsg); //Print initial message
+		Tree start = new Tree(0);
+		ArrayList<Question> initial = start.getNextQuestion();//
+		initial.get(0).printQuestion();//print the initial question
 		UserInput ui= new UserInput(); //Create instance of User Input class
 		user = ui.getInput();	//Read user input via Scanner in UI class
 		
@@ -20,15 +20,12 @@ public class Testing {
 		if(user.contains("phone")) {selection = 3;} //phone = tree #3
 		
 		
-		Tree<String> bot = new Tree<String>(selection); //the tree created from user's input
-		ArrayList<Question> questions = bot.getNextQuestion();
+		Tree bot = new Tree(selection); //the tree created from user's input
+		ArrayList<Question> questions = bot.getNextQuestion(); //ArrayList that hold the questions
 		for(int i=0; i<questions.size(); i++) {
-		questions.get(i).printQuestion();
+		questions.get(i).printQuestion(); //get next question
 		user = ui.getInput(); //read user input
-		}
-		
-		//do next steps based on which tree is created
-		
+		}		
 	}
 
 }
--- src/Tree.java
+++ src/Tree.java
@@ -7,35 +7,33 @@ package src;
 import java.util.ArrayList;
 
 
-public class Tree<T>{
+public class Tree{
 	    private Node<String> root;
 	    
 		public Tree(int i) { //specific constructor for the 3 main trees
 		  	if(i==0) {//start
-		  		root = new Node<String>("Is your issue in regards to Internet, TV, or phone?");
-		  		root.data = "Is your issue in regards to Internet, TV, or phone?";
-		  		root.children = null;
-		  	}
-		  	else if(i==1) { //Internet
-		  		String initial = "Are you connected to wifi or ethernet?";
+		  		String initial = "Is your issue in regards to Internet, TV, or phone?";
 		  		root = new Node<String>(initial);
 		  		root.data = initial;
+		  		root.qb = new QuestionBuilder("Start");
+		  		root.qb.buildQuestions();
+		  		root.children = root.qb.getBuiltQuestions();
+		  	}
+		  	else if(i==1) { //Internet
+		  		root = new Node<String>("Are you connected to wifi or ethernet?");
 		  		root.qb = new QuestionBuilder("Internet");
 		  		root.qb.buildQuestions();
 		  		root.children = root.qb.getBuiltQuestions();
 		  	}
 		  	else if(i==2) {//TV
-		  		String initial = "Are you watching Cable or Netflix?";
-		  		root = new Node<String>(initial);
-		  		root.data = initial;
+		  		root = new Node<String>("Are you watching Cable or Netflix?");
 		  		root.qb = new QuestionBuilder("TV");
 		  		root.children = root.qb.getBuiltQuestions();
 		  	}
 		  	else if(i==2) {//Phone
-		  		String initial = "Are you using a Mobile phone or Landline phone?";
-		  		root = new Node<String>(initial);
-		  		root.data = initial;
-		  		root.children = new ArrayList<Question>();
+		  		root = new Node<String>("Are you using a Mobile phone or Landline phone?");
+		  		root.qb = new QuestionBuilder("Phone");
+		  		root.children = root.qb.getBuiltQuestions();
 		  		
 		  	}
 
@@ -43,6 +41,7 @@ public class Tree<T>{
 	    public ArrayList<Question> getNextQuestion() {
 	    	return root.qb.getBuiltQuestions();
 	    }
+	    
 	    
 	    public void add(String question) {
 	    	if(root==null) {
@@ -58,7 +57,7 @@ public class Tree<T>{
 	    		//root.children.add(new Question(n)); //add new node to root's child array list
 	    	}
 	    }
-	    public static class Node<T> {
+	    public static class Node<T> { //basic node class
 	        private T data;
 			private Node<T> parent;
 	        private QuestionBuilder qb;
@@ -69,7 +68,4 @@ public class Tree<T>{
 	        	parent = null;
 	        }
 	    }
-		public String getRootData() {
-			return root.data;
-		}
 }
