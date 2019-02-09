7f3ab88dc2d2fe926a24d2c61b2c689b68b1018b
DamynFilipuzzi <damynfilipuzzi@gmail.com>
2019-02-08 17:35:39 -0800

    Update Decision_Matrix.java

--- src/Decision_Matrix.java
+++ src/Decision_Matrix.java
@@ -1,9 +1,15 @@
 public class Decision_Matrix {
 
   public Decision() {
-
+    Hasmap<int, String> keywords = new Hasmap<int, String>;
+    Hasmap<int, String> userInput = new Hashmap<int, String>;
     switch () {
+      case() {
 
+      }
+      case() {
+
+      }
     }
 
   }
