122d9c41fc11e44d1d5a5744d367d3951028168a
Daulton <daultonbaird1@gmail.com>
2019-02-08 19:17:35 -0800

    Modified Questions and Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -12,40 +12,37 @@ public class Tree{
 	    
 		public Tree(int i) { //specific constructor for the 3 main trees
 		  	if(i==0) {//start
-		  		String initial = "Is your issue in regards to Internet, TV, or phone?";
-		  		root = new Node<String>(initial);
-		  		root.data = initial;
-		  		root.qb = new QuestionBuilder("Start");
-		  		root.qb.buildQuestions();
-		  		root.children = root.qb.getBuiltQuestions();
+		  		root = new Node<String>("Is your issue in regards to Internet, TV, or phone?"); //creates a new node for the root
+		  		root.qb = new QuestionBuilder("Start"); //Start stores the question(s) for the first Tree
+		  		root.qb.buildQuestions(); //load questions into array
+		  		root.children = root.qb.getBuiltQuestions(); //load built questions into children
 		  	}
 		  	else if(i==1) { //Internet
-		  		root = new Node<String>("Are you connected to wifi or ethernet?");
-		  		root.qb = new QuestionBuilder("Internet");
-		  		root.qb.buildQuestions();
-		  		root.children = root.qb.getBuiltQuestions();
+		  		root = new Node<String>("Are you connected to wifi or ethernet?"); //load initial into new node
+		  		root.qb = new QuestionBuilder("Internet"); //Internet folder stores questions
+		  		root.qb.buildQuestions(); //load questions into array
+		  		root.children = root.qb.getBuiltQuestions(); //load built questions into children
 		  	}
 		  	else if(i==2) {//TV
-		  		root = new Node<String>("Are you watching Cable or Netflix?");
-		  		root.qb = new QuestionBuilder("TV");
-		  		root.children = root.qb.getBuiltQuestions();
+		  		root = new Node<String>("Are you watching Cable or Netflix?"); //load initial into new node
+		  		root.qb = new QuestionBuilder("TV"); //TV folder stores questions
+		  		root.children = root.qb.getBuiltQuestions(); //load built questions into children
 		  	}
 		  	else if(i==2) {//Phone
-		  		root = new Node<String>("Are you using a Mobile phone or Landline phone?");
-		  		root.qb = new QuestionBuilder("Phone");
-		  		root.children = root.qb.getBuiltQuestions();
+		  		root = new Node<String>("Are you using a Mobile phone or Landline phone?"); //load initial into new node
+		  		root.qb = new QuestionBuilder("Phone"); //Phone folder stores questions
+		  		root.children = root.qb.getBuiltQuestions(); //load built questions into children
 		  	}
 
 		}
 	    public ArrayList<Question> getNextQuestion() {
-	    	return root.qb.getBuiltQuestions();
+	    	return root.qb.getBuiltQuestions(); //returns ArrayList of built questions
 	    }
 	    
 	    
 	    public void add(String question) {
 	    	if(root==null) {
 	    		root= new Node<String>(question); //create root node based on data
-	    		root.data = question; 
 	    		root.children = new ArrayList<Question>(); //Initialize children ArrayList
 	    	}
 	    	else { //root is already set to something
