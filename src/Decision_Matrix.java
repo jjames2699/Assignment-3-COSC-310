abf40c03d6d58fc32fed64538a255b4ec5500e39
RandyYunjaeLee <43257396+RandyYunjaeLee@users.noreply.github.com>
2019-02-08 19:28:59 -0800

    test plz

--- src/Decision_Matrix.java
+++ src/Decision_Matrix.java
@@ -1,25 +1,41 @@
 package src;
 
+import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class Decision_Matrix {
-
+  public static void main(String[] args) {
+    ArrayList<String> s = new ArrayList<String>();
+    s.add("hi");
+    s.add("yes");
+    HashMap<Integer, String> h = getHashValue(s);
+    System.out.println(s);
+    System.out.print(h.toString());
+  }
 
   public int Decision() {
-    Integer decision = 0;
+    int decision = 0;
 
-    switch () {
+    switch (decision) {
       case 1:
 
+      break;
       case 2:
 
+      break;
       }
       return decision;
     }
 
-  private HashMap<Integer, String> getHashValue(ArrayList<String> list) {
+  private static HashMap<Integer, String> getHashValue(ArrayList<String> list) {
     HashMap<Integer, String> hash = new HashMap<Integer, String>();
-  
+    Iterator<String> it = list.iterator();
+    while(it.hasNext()){
+      String str = it.next();
+      Integer i = new Integer(str.hashCode());
+      hash.put(i, str);
+    }
     return hash;
   }
+
 }
