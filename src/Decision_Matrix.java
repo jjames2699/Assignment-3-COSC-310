dbaabf2434a280e44e0c323ef24e72c2489664f8
DamynFilipuzzi <damynfilipuzzi@gmail.com>
2019-02-08 18:16:16 -0800

    Update Decision_Matrix.java

--- src/Decision_Matrix.java
+++ src/Decision_Matrix.java
@@ -1,13 +1,10 @@
 package src;
 public class Decision_Matrix {
 
-
   public int Decision() {
     Integer decision = 0;
-    Hashmap<Integer, String> keywords = new Hashmap<Integer, String>();
-    Hashmap<Integer, String> userInput = new Hashmap<Integer, String>();
-
-
+    HashMap<Integer, String> keywords = new HashMap<Integer, String>();
+    HashMap<Integer, String> userInput = new HashMap<Integer, String>();
 
     switch () {
       case 1:
