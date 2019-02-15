b79a029743fdbc995ff70d589599c36e1b50b4a3
RandyYunjaeLee <43257396+RandyYunjaeLee@users.noreply.github.com>
2019-02-15 12:38:48 -0800

    more helpers

--- src/Decision_Matrix.java
+++ src/Decision_Matrix.java
@@ -3,46 +3,73 @@ package src;
 //started Feb 8 worked for 1 hour
 //Feb 12 worked on it for 3 hours
 //Feb 13 worked on it for 3 hours
+//Feb 15 worked on it from 11:30 -
 //note to self spilt up into helper methods for unit testing
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class Decision_Matrix {
-  String[] list;
   String tryAgain = "Did not understand that, please try again.";
   //inputs are userInput, filename
   //note number of answer = 4th line in txt file
   public String[] letsSplit(String s){
     String[] files_part1 = s.split("\\.");
     String[] files_part2 = files_part1[0].split("-");
-    String[] split = {files_part1[0],files_part2[0],files_part2[1]};
+    String[] split = {files_part2[0],files_part2[0],files_part1[1]};
     return split;
   }
-  public String anythingElse(){
-    for(int j = 0; j<list.length; j++){
+  //for asking if there is anything else
+  public String anythingElse(String userInput){
       //if the word says answer 1 point to begin the loop again
-      if(list[j].contains("yes")){
+      if(userInput.contains("yes")){
         return "loop-0.txt";
       }
       //if the word says answer 2 point to end the program
-      else if(list[j].contains("no")){
+      else if(userInput.contains("no")){
         return "end-0.txt";
       }else {
         System.out.println(tryAgain);
         return "anythingElse-0.txt";
       }
+  }
+  public String[] threeOrTwo(int i, String userInput, String[] answers, String[] files_part2, String file){
+    if(i==3){
+      //if the word says answer 1 add a 0 to the path
+      if(userInput.equalsIgnoreCase(answers[0].split(" ")[1])){
+        files_part2[0]=files_part2[0]+"0";
+        //don't need to check anymore
+        //if the word says answer 2 add a 1 to the path
+      }else if(userInput.equalsIgnoreCase(answers[1].split(" ")[1])){
+        files_part2[0]=files_part2[0]+"1";
+        //if the word says answer 3 add a 2 to the path
+      }else if(userInput.equalsIgnoreCase(answers[2].split(" ")[1])){
+        files_part2[0]= files_part2[0]+"2";
+      }else {
+        System.out.println(tryAgain);
+        return letsSplit(file);
+      }
+      files_part2[1]="0";
+    }else if(i==2){
+      //if the word says answer 1 add a 0 to the path
+      if(userInput.equalsIgnoreCase(answers[0].split(" ")[1])){
+        files_part2[0]=files_part2[0]+"0";
+      }
+      //if the word says answer 2 add a 1 to the path
+      else if(userInput.equalsIgnoreCase(answers[1].split(" ")[1])){
+        files_part2[0]=files_part2[0]+"1";
+      }else{
+        System.out.println(tryAgain);
+        return letsSplit(file);
+      }
+      //change priority
+      files_part2[1]="0";
     }
-    return null;
+    return files_part2;
   }
   public String Decision(String userInput, String file, int selection) throws FileNotFoundException {
     String decision;
-    //make the user input lowercase
-    //userInput = userInput.toLowerCase();
-    //this line makes the string into an array of string separated at every space
-    this.list = userInput.split(" ");
     //these 2 lines separates the file name by - and .
-    String[] files_part1 = file.split("\\.");
-    String[] files_part2 = files_part1[0].split("-");
+    String[] files_part2 = letsSplit(file);
     //check selection
     File f = new File("");
     if(selection==1){
@@ -75,75 +102,21 @@ public class Decision_Matrix {
     if(check.contains("Did that"))
     {
       //if the word says answer 1 point to begin the loop again
-      if(list[0].contains(answers[0].split(" ")[1])) { //if they say yes
+      if(userInput.contains(answers[0].split(" ")[1])) { //if they say yes
         return "anythingElse-0.txt";
       }
     }
     if(check.contains("anything else"))
     {
-      for(int j = 0; j<list.length; j++){
-        //if the word says answer 1 point to begin the loop again
-        if(list[j].contains("yes")){
-          return "loop-0.txt";
-        }
-        //if the word says answer 2 point to end the program
-        else if(list[j].contains("no")){
-          return "end-0.txt";
-        }else {
-          System.out.println(tryAgain);
-          return file;
-        }
-      }
-
+        return anythingElse(userInput);
     }
-      if(i==3){
-        for(int j = 0; j<list.length; j++){
-          //if the word says answer 1 add a 0 to the path
-          if(list[j].equalsIgnoreCase(answers[0].split(" ")[1])){
-            files_part2[0]=files_part2[0]+"0";
-            //don't need to check anymore
-            break;
-            //if the word says answer 2 add a 1 to the path
-          }else if(list[j].equalsIgnoreCase(answers[1].split(" ")[1])){
-            files_part2[0]=files_part2[0]+"1";
-            break;
-            //if the word says answer 3 add a 2 to the path
-          }else if(list[j].equalsIgnoreCase(answers[2].split(" ")[1])){
-            files_part2[0]= files_part2[0]+"2";
-            break;
-          }else {
-            System.out.println(tryAgain);
-            return file;
-          }
-        }
-        files_part2[1]="0";
-      }else if(i==2){
-
-        for(int j = 0; j<list.length; j++){
-          //if the word says answer 1 add a 0 to the path
-          if(list[j].equalsIgnoreCase(answers[0].split(" ")[1])){
-            files_part2[0]=files_part2[0]+"0";
-            break;
-          }
-          //if the word says answer 2 add a 1 to the path
-          else if(list[j].equalsIgnoreCase(answers[1].split(" ")[1])){
-            files_part2[0]=files_part2[0]+"1";
-            break;
-          }else{
-            System.out.println(tryAgain);
-            return file;
-          }
-        }
-        //change priority
-        files_part2[1]="0";
-      }
-      else if(i==1){
-        //new priority if empty question
-        files_part2[1]="1";
-      }
-
-      //decision = path, priority and file type
-      decision = files_part2[0]+"-"+files_part2[1]+"."+files_part1[1];
-      return decision;
+    files_part2 = threeOrTwo(i, userInput, answers, files_part2, file);
+    if(i==1){
+      //new priority if empty question
+      files_part2[1]="1";
+    }
+    //decision = path, priority and file type
+    decision = files_part2[0]+"-"+files_part2[1]+".txt";
+    return decision;
   }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -4,7 +4,6 @@
  */
 package src;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
