a8ddc247c30da9782bd8cef607c61ac83b39d22b
Daulton <daultonbaird1@gmail.com>
2019-02-15 11:41:05 -0800

    make initial msg loop if msg invalid

--- src/Testing.java
+++ src/Testing.java
@@ -12,14 +12,17 @@ public class Testing {
 			//BotTree bot = new BotTree(0); //This is going to be the initial instance of whatever tree class we make
 			Tree start = new Tree(0);
 			ArrayList<Question> initial = new ArrayList<>(start.getNextQuestion().values());//
-			initial.get(0).printQuestion();//print the initial question
-			UserInput ui= new UserInput(); //Create instance of User Input class
-			String user = ui.getInput();	//Read user input via Scanner in UI class
 			int selection = 0; //select which tree to go to based on user's input
-			if(user.contains("internet")) {selection = 1;} //internet = tree #1
-			else if(user.contains("phone")) {selection = 2;} //phone = tree #2
+			UserInput ui; String user= "";
+			while(true) {
+			initial.get(0).printQuestion();//print the initial question
+			ui= new UserInput(); //Create instance of User Input class
+			user = ui.getInput();	//Read user input via Scanner in UI class
+			if(user.contains("internet")) {selection = 1; break;} //internet = tree #1
+			else if(user.contains("phone")) {selection = 2; break;} //phone = tree #2
 			//else if(user.contains("tv")) {selection = 3;} //tv = tree #3
 			else {System.out.println("Entry invalid, try again");}
+			}
 			Tree bot = new Tree(selection); //the tree created from user's input
 			//standard file opening
 			String file = "0-0.txt";
