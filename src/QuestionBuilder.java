e457828f262c99063906e0803ae38c9c578a6172
Daulton <daultonbaird1@gmail.com>
2019-02-15 10:31:16 -0800

    i fixed it

--- src/QuestionBuilder.java
+++ src/QuestionBuilder.java
@@ -60,7 +60,7 @@ public class QuestionBuilder {
 				temp.setQuestion(s.nextLine());
 				
 				//Read inputRange
-				tempLine = s.nextLine();
+				//tempLine = s.nextLine(); //commenting this fixes it idk why...
 				temp.setInputRange(Integer.parseInt(tempLine));
 				
 				//Read Answers
