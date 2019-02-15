a6b98a2eb4381cacc522352d55ac7c69ade5c5a5
RandyYunjaeLee <43257396+RandyYunjaeLee@users.noreply.github.com>
2019-02-14 20:39:39 -0800

    starting

--- src/Decision_Matrix.java
+++ src/Decision_Matrix.java
@@ -18,8 +18,8 @@ public class Decision_Matrix {
     //this line makes the string into an array of string separated at every space
     String[] list = userInput.split(" ");
     //these 2 lines separates the file name by - and .
-    String[] list2 = file.split("\\.");
-    String[] ans = list2[0].split("-");
+    String[] files_part1 = file.split("\\.");
+    String[] files_part2 = files_part1[0].split("-");
     //check selection
     File f = new File("");
     if(selection==1){
@@ -49,6 +49,13 @@ public class Decision_Matrix {
     }
 
     int i = Integer.parseInt(s1);
+    if(check.contains("Did that"))
+    {
+      //if the word says answer 1 point to begin the loop again
+      if(list[0].contains(answers[0].split(" ")[1])) { //if they say yes
+        return "anythingElse-0.txt";
+      }
+    }
     if(check.contains("anything else"))
     {
         //if the word says answer 1 point to begin the loop again
@@ -67,47 +74,34 @@ public class Decision_Matrix {
         for(int j = 0; j<list.length; j++){
           //if the word says answer 1 add a 0 to the path
           if(list[j].equalsIgnoreCase(answers[0].split(" ")[1])){
-            ans[0]=ans[0]+"0";
+            files_part2[0]=files_part2[0]+"0";
             //don't need to check anymore
             break;
             //if the word says answer 2 add a 1 to the path
           }else if(list[j].equalsIgnoreCase(answers[1].split(" ")[1])){
-            ans[0]=ans[0]+"1";
+            files_part2[0]=files_part2[0]+"1";
             break;
             //if the word says answer 3 add a 2 to the path
           }else if(list[j].equalsIgnoreCase(answers[2].split(" ")[1])){
-            ans[0]= ans[0]+"2";
+            files_part2[0]= files_part2[0]+"2";
             break;
           }else {
             System.out.println(tryAgain);
             return file;
           }
         }
-        ans[1]="0";
+        files_part2[1]="0";
       }else if(i==2){
-    	  if(check.contains("Did that"))
-    	    {
-    	        //if the word says answer 1 point to begin the loop again
-    		  if(list[0].contains(answers[0].split(" ")[1])){ //if they say yes
-    	          return "anythingElse-0.txt";
-    	        }
-    	        //if the word says answer 2 point to end the program
-    	        else if(list[0].contains(answers[1].split(" ")[1])){ //if they say no
-    	        		//nothing here so continues
-    	        }else {
-    	          System.out.println(tryAgain);
-    	          return file;
-    	        }
-    	    }
+
         for(int j = 0; j<list.length; j++){
           //if the word says answer 1 add a 0 to the path
           if(list[j].equalsIgnoreCase(answers[0].split(" ")[1])){
-            ans[0]=ans[0]+"0";
+            files_part2[0]=files_part2[0]+"0";
             break;
           }
           //if the word says answer 2 add a 1 to the path
           else if(list[j].equalsIgnoreCase(answers[1].split(" ")[1])){
-            ans[0]=ans[0]+"1";
+            files_part2[0]=files_part2[0]+"1";
             break;
           }else{
             System.out.println(tryAgain);
@@ -115,11 +109,11 @@ public class Decision_Matrix {
           }
         }
         //change priority
-        ans[1]="0";
+        files_part2[1]="0";
       }
       else if(i==1){
         //new priority if empty question
-        ans[1]="1";
+        files_part2[1]="1";
       }
       else if(i==0){
         //ends program
@@ -128,10 +122,10 @@ public class Decision_Matrix {
       }
       else if(i==-1){
         //return the first file in the sequence
-        return "0-0.txt";
+        return "false";
       }
       //decision = path, priority and file type
-      decision = ans[0]+"-"+ans[1]+"."+list2[1];
+      decision = files_part2[0]+"-"+files_part2[1]+"."+files_part1[1];
       return decision;
   }
 }
