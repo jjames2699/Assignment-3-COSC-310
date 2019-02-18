3888a435b6707793fcc4ab6467eff02a659a2fa1
Daulton <daultonbaird1@gmail.com>
2019-02-17 20:19:53 -0800

    Fixed tests. Someone test 4 me pls

--- UnitTesting/TreeTest.java
+++ UnitTesting/TreeTest.java
@@ -1,29 +1,44 @@
 package UnitTesting;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Test;
 
 import src.Question;
 import src.Tree;
 
 /*Unit Test for Tree Class that creates an instance of each tree and prints 1 question from it.
- * This is done the same way as in the Run Class
+ * 
  * Author: Daulton Baird
  */
 
 public class TreeTest {
 	
-	public static void main(String[] args) {
+	@Test
+	void TestTree0() {
+		String initial = "Is your issue in regards to internet, or phone?";
 		Tree tree0 = new Tree(0);
 		ArrayList<Question> list = new ArrayList<>(tree0.getNextQuestion().values());
-		list.get(0).printQuestion();
-		
+		assertEquals(initial, list.get(0).getQuestion());
+	}
+	@Test
+	void TestTree1() {
+		String initial ="Are you connected to Wifi or Ethernet?";
 		Tree tree1 = new Tree(1);
-		ArrayList<Question> list1 = new ArrayList<>(tree1.getNextQuestion().values());
-		list1.get(0).printQuestion();
-		
+		HashMap<String, Question> questions = tree1.getNextQuestion();
+		questions.get("0-0.txt").getQuestion();
+		assertEquals(initial, questions.get("0-0.txt").getQuestion());
+	}
+	@Test
+	void TestTree2() {
+		String initial = "Are you using a landline or a cell phone?";
 		Tree tree2 = new Tree(2);
-		ArrayList<Question> list2 = new ArrayList<>(tree2.getNextQuestion().values());
-		list2.get(1).printQuestion();
+		HashMap<String, Question> questions = tree2.getNextQuestion();
+		questions.get("0-0.txt").getQuestion();
+		assertEquals(initial, questions.get("0-0.txt").getQuestion());
 	}
 
 }
